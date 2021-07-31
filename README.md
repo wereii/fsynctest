3176b97ac7276a2966476274b82031e27b506a40
wereii <8504482+wereii@users.noreply.github.com>
2021-07-31 22:35:51 +0200

    Better documentation

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## How To
 
 - Install `make` and `gcc` packages
-- Pull this repository and `cd` into it
+- Clone this repository and `cd` into it
 - run `make`
 - :money_with_wings: (`FSYNC: OK` means the patch is there, `FSYNC: FAIL` means no bueno)
 - after running `make` at least once you can recheck with `./check.sh`
--- fsync_syscall.c
+++ fsync_syscall.c
@@ -1,11 +1,13 @@
 #include <errno.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/syscall.h>
 
 static const struct timespec zero;
 
-int main(){
-	syscall(__NR_futex, NULL, 31, 0, &zero, 0, 0);
-	return errno == ENOSYS;
+int main() {
+    // https://github.com/ValveSoftware/wine/blob/3da025c3b623c415f61e3200c4ec77554e1ab7d7/server/fsync.c#L63
+    syscall(__NR_futex, NULL, 31, 0, &zero, 0, 0);
+    return errno == ENOSYS;
 }
